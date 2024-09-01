e14344870cc75bc3fb52ca646ae0104db0d32340
ggang89 <ggang89@gmail.com>
2024-09-01 23:01:14 +0900

    state에서 중첩된 객체 업데이트하기

--- src/useState.js
+++ src/useState.js
@@ -2,8 +2,46 @@ import { useState } from "react";
 
 export default function UseState() {
   const [like, setLike] = useState(true);
+  const [counter, setCounter] = useState(0);
+  const [form, setForm] = useState({
+    name: "psy",
+    detail: {
+      gender: "woman",
+      age:35
+    }
+  })
   const handleChange = (e) => {
     setLike(e.target.checked);
+  };
+  const increase = () => {
+    setCounter((c) => c + 1);
+    //setCounter(counter +1)이 아니라 업데이트 함수로 전달!
+    //state가 이전 state에서 계산되어서 사용됨
+  };
+  
+  const nameChange = (e) => {
+    setForm({
+      ...form,
+      name: e.target.value
+    })
+  }
+  const genderChange = (e) => {
+    setForm({
+      ...form,
+      detail: {
+        ...form.detail,
+        gender: e.target.value
+      }
+    })
+  }
+  const ageChange = (e) => {
+    setForm({
+      ...form,
+      detail:{
+        ...form.detail,
+        age: e.target.value
+      }
+    })
   }
   return (
     <>
@@ -12,6 +50,25 @@ export default function UseState() {
         ❤Like
       </label>
       <p>I {like ? "Like this." : "Don't Like this"}</p>
+      
+      <p>
+        {counter}{" "}
+        <button
+          onClick={() => {
+            increase();
+            increase();
+            increase();
+          }}
+        >
+          +3
+        </button>
+      </p>
+      <form>
+        <label>Name: <input value={form.name} onChange={nameChange}/></label>
+        <label>Gender:<input value={form.detail.gender} onChange={genderChange} /></label>
+        <label>Age:<input value={form.detail.age} onChange={ageChange}/></label>
+      </form>
+
     </>
   );
 }
